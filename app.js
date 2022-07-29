b211af59cf63ee1e2394631041626a25fd71001d
Hussein66253 <hussein66253@icloud.com>
2022-07-29 22:25:53 +0300

    added allCountries route

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 const api = require("./routes/api");
 
 app.use(morgan("dev"));
-
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
 app.use(express.json());
 app.get("/", (req, res) => {
   res.json({
